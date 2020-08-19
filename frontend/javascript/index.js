a43b91d31d34423fa0b38afb83515803e6ba8977
ParamagicDev <konnor5456@gmail.com>
2020-08-18 21:59:35 -0400

    will debug tommorow

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,11 @@
 import "../styles/index.scss"
+import "../styles"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Turbolinks from "turbolinks"
 
 console.info("Bridgetown is loaded!")
+const application = Application.start()
+const context = require.context("./controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
+Turbolinks.start()
---
+++ snowpack.config.js
@@ -0,0 +1,46 @@
+const mount = {
+  frontend: "/_frontend_",
+  output: "/",
+};
+
+const proxy = {
+  // Account for users using Docker
+  "0.0.0.0:4000": "0.0.0.0:4000",
+};
+
+const plugins = [
+  [
+    "@snowpack/plugin-run-script",
+    {
+      cmd: "bundle exec bridgetown build",
+      watch: "$1 --watch",
+    },
+  ],
+  ["@snowpack/plugin-webpack"],
+];
+
+const installOptions = {
+  NODE_ENV: true,
+};
+
+const devOptions = {
+  // Default port of bridgetown
+  port: 4000,
+  open: "none",
+  // Default output directory of Bridgetown
+  out: "output",
+};
+
+const buildOptions = {
+  clean: true,
+};
+
+module.exports = {
+  mount,
+  proxy,
+  plugins,
+  installOptions,
+  devOptions,
+  buildOptions,
+};
+
